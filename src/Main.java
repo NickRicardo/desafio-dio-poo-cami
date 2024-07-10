35ccafa3eed42d6e4b6933b3483ad3a9516993d1
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-07-10 16:53:21 -0300

    Herança e Polimorfismo

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.setTitulo("Sofrendo com POO");
+        curso.setDescricao("Descrição curso java");
+        curso.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Sofrendo com API");
+        curso2.setDescricao("Descrição curso java api");
+        curso2.setCargaHoraria(8);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
